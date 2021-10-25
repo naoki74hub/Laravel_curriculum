f393797a2dd344b58ba5e7e6ea58c869cbdb0b14
naoki74 <nao.ryotaro.1112@icloud.com>
2021-10-25 15:04:57 +0900

    [add]詳細画面追加

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -13,16 +13,12 @@
    </head>
     <body>
         <h1>Blog Name</h1>
-        <div class="posts">
-            @foreach($posts as $post)
-            <div class="post">
-                <a href="/posts/{{$post->id}}"><h2 class="title">{{$post->title}}</h2></a>
+        <div class="post">
+          <h2 class="title">{{$post->title}}</h2>
                 <p class="body">{{$post->body}}</p>
-            </div>
-            @endforeach
-        </div>
-        <div class="paginate">
-            {{$posts->links()}}
-            </div>
-    </body>
+                <p class="updated_at">{{$post->updated_at}}</p>
+                </div>
+                 <div class="back"><a href="/">back</a></div> //
+           </body>
+           
 </html>
